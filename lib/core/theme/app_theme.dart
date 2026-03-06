28f8b8cb82b70f80527cef0abca39a8ac6f23d78
MixKage <shishov265@gmail.com>
2026-03-06 15:59:27 +0300

    add: new neon style tooltip

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -19,6 +19,36 @@ class AppTheme {
           fontWeight: FontWeight.w700,
         ),
       ),
+      tooltipTheme: TooltipThemeData(
+        triggerMode: TooltipTriggerMode.longPress,
+        waitDuration: const Duration(milliseconds: 420),
+        showDuration: const Duration(milliseconds: 2200),
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        margin: const EdgeInsets.symmetric(horizontal: 14),
+        textStyle: GoogleFonts.manrope(
+          color: const Color(0xFFE9EEFF),
+          fontSize: 12,
+          fontWeight: FontWeight.w700,
+          letterSpacing: 0.1,
+        ),
+        decoration: BoxDecoration(
+          color: const Color(0xEE11152A),
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(color: const Color(0x88798CFF), width: 1.1),
+          boxShadow: const <BoxShadow>[
+            BoxShadow(
+              color: Color(0x66A16BFF),
+              blurRadius: 16,
+              spreadRadius: 0.8,
+            ),
+            BoxShadow(
+              color: Color(0x553FCBFF),
+              blurRadius: 14,
+              spreadRadius: 0.6,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
